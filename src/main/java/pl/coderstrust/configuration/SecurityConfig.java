dea188c657db626a370a99320c0e1170e99082dc
Piotr Górowski <piotrg2@tlen.pl>
2019-10-09 13:02:21 +0200

    Basic frontend

--- src/main/java/pl/coderstrust/configuration/SecurityConfig.java
+++ src/main/java/pl/coderstrust/configuration/SecurityConfig.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.PropertySource;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
 
 @Configuration
 @PropertySource("classpath:application.properties")
@@ -36,10 +35,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         .permitAll()
         .and()
         .logout()
-        .permitAll()
-        .and()
-        .sessionManagement()
-        .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        .permitAll();
   }
 
   @Autowired
